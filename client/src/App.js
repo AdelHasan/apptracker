04c25cc112f77566979460bfe8295de44f224c99
Adel Hasan <adelhasan@Adels-MacBook-Pro.local>
2022-06-28 13:12:48 -0500

    added register page route to landing page

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,17 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <nav>
+          <Link to='/'>Dashboard</Link>
+          <Link to='/register'>Register</Link>
+          <Link to='/landing'>Landing</Link>
+        </nav>
       <Routes>
         <Route path='/' element={<div>Dashboard</div>} />
         <Route path='/register' element={<div>Register</div>} />
         <Route path='/landing' element={<Landing />} />
         <Route path='*' element={<h1>Error</h1>} />
       </Routes>
-
-      
       </Router>
     </div>
   );
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,6 +2,7 @@ import {Logo} from '../components'
 import main from '../assets/images/mainalt.svg'
 import styled from 'styled-components'
 import Wrapper from '../assets/wrappers/LandingPage'
+import {Link} from 'react-router-dom'
 
 const Landing = () => {
   return (
@@ -16,7 +17,7 @@ const Landing = () => {
             job <span>tracking</span> tool
           </h1>
           <p>You know, I was God once. Yeah. Give a little credit to our public schools. It's just like the story of the grasshopper and the octopus. All year long, the grasshopper kept burying acorns for winter, while the octopus mooched off his girlfriend and watched TV. But then the winter came, and the grasshopper died, and the octopus ate all his acorns. Also he got a race car. Is any of this getting through to you?</p>
-          <button className='btn btn-hero'>Login / Register</button>
+          <Link to='/register' className='btn btn-hero'>Login / Register</Link>
         </div>
         <img src={main} alt='job seeker' className='img main-img' />
       </div>
