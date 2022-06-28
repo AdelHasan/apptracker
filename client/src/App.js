c995fa341a97c9f737651cf49ac802f979fa1b3b
Adel Hasan <adelhasan@Adels-MacBook-Pro.local>
2022-06-28 13:25:21 -0500

    page structure for react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,17 @@
-import Landing from "./pages/Landing";
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import {Landing, Dashboard, Register, Error} from './pages'
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <nav>
-          <Link to='/'>Dashboard</Link>
-          <Link to='/register'>Register</Link>
-          <Link to='/landing'>Landing</Link>
-        </nav>
+        
       <Routes>
-        <Route path='/' element={<div>Dashboard</div>} />
-        <Route path='/register' element={<div>Register</div>} />
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/register' element={<Register />} />
         <Route path='/landing' element={<Landing />} />
-        <Route path='*' element={<h1>Error</h1>} />
+        <Route path='*' element={<Error />} />
       </Routes>
       </Router>
     </div>
