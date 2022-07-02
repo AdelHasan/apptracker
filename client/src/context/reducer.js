23513050bc837b96940e022d80ea7c97bf45b07c
Adel Hasan <adelhasan9947@gmail.com>
2022-07-02 17:53:39 -0500

    clear alert reducer

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -1,4 +1,4 @@
-import { DISPLAY_ALERT } from "./actions";
+import { DISPLAY_ALERT, CLEAR_ALERT } from "./actions";
 
 const reducer = (state, action) => {
   if (action.type === DISPLAY_ALERT) {
@@ -8,6 +8,15 @@ const reducer = (state, action) => {
       alertType: "danger",
       alertText: "Please complete all fields!",
     };
+  }
+
+  if (action.type === CLEAR_ALERT) {
+    return {
+      ...state,
+      showAlert: false,
+      alertType: "",
+      alertText: "",
+    }
   }
 
   throw new Error(`no such action : ${action.type}`);
