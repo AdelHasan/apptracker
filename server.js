c2d450c7883bb690bc961f4fe8d5c1465be8420c
Adel Hasan <adelhasan9947@gmail.com>
2022-07-05 19:21:45 -0500

    not found middleware

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import express from 'express'
+import notFoundMiddleware from './middleware/not-found.js'
 
 const app = express()
+
+// middleware
+notFoundMiddleware
 
 app.get('/', (req,res) => {
   res.send('Hello World')
 })
+
+app.use(notFoundMiddleware)
 
 const port = process.env.PORT || 5000
 
