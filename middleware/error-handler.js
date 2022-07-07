ba29782bde58a06e96346abca1b777170a5685df
Adel Hasan <adelhasan9947@gmail.com>
2022-07-07 12:23:21 -0500

    updated error message for non-unique email value

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -11,6 +11,11 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     // defaultError.msg = err.message
     defaultError.msg = Object.values(err.errors).map((item) => item.message).join(", ")
   }
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST
+    defaultError.msg = `FIELD: ${Object.keys(err.keyValue)} MUST BE UNIQUE`
+  }
+  // V Default Error V
   // res.status(defaultError.statusCode).json({msg: err})
   res.status(defaultError.statusCode).json({msg:defaultError.msg})
 
