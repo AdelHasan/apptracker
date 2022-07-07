7e97412b5cc45e277cbba5cb8053da5a0dffca94
Adel Hasan <adelhasan9947@gmail.com>
2022-07-07 12:52:23 -0500

    CustomAPIError class for error statusCodes and msgs

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,18 @@
 import User from "../models/User.js"
 import { StatusCodes } from "http-status-codes"
 
+class CustomAPIError extends Error {
+  constructor(message) {
+    super(message)
+    this.statusCode = StatusCodes.BAD_REQUEST
+  }
+}
+
 const register = async (req, res, next) => {
     const {name, email, password} = req.body
 
     if (!name || !email || !password) {
-      throw new Error('PLEASE COMPLETE ALL FIELDS')
+      throw new CustomAPIError('PLEASE COMPLETE ALL FIELDS')
     }
 
 
