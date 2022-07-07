fc07b13c85325a7f79a38739cf1c04d538c5ae2a
Adel Hasan <adelhasan9947@gmail.com>
2022-07-07 17:10:11 -0500

    remove password from create user response json

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,32 +1,39 @@
-import User from "../models/User.js"
-import { StatusCodes } from "http-status-codes"
-import { BadRequestError, NotFoundError} from '../errors/index.js'
-
-
+import User from "../models/User.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, NotFoundError } from "../errors/index.js";
 
 const register = async (req, res, next) => {
-    const {name, email, password} = req.body
+  const { name, email, password } = req.body;
 
-    if (!name || !email || !password) {
-      throw new BadRequestError('PLEASE COMPLETE ALL FIELDS')
-    }
+  if (!name || !email || !password) {
+    throw new BadRequestError("PLEASE COMPLETE ALL FIELDS");
+  }
 
-    const userAlreadyExists = await User.findOne({email})
-    if (userAlreadyExists) {
-      throw new BadRequestError('Email already in use')
-    }
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError("Email already in use");
+  }
 
-    const user = await User.create({name, email, password})
-    const token = user.createJWT()
-    res.status(StatusCodes.CREATED).json({user, token})
-}
+  const user = await User.create({ name, email, password });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({
+    user : {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name
+    },
+    token,
+    location: user.location
+  });
+};
 
 const login = async (req, res) => {
-  res.send('login user')
-}
+  res.send("login user");
+};
 
 const updateUser = async (req, res) => {
-  res.send('update user')
-}
+  res.send("update user");
+};
 
-export {register, login, updateUser}
+export { register, login, updateUser };
