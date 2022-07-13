ef1cf6f4d777a3beea7743a947c449a36aa11623
Adel Hasan <adelhasan9947@gmail.com>
2022-07-13 17:09:02 -0500

    layout, placeholder navbar

--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -1,6 +1,15 @@
+import { Outlet, Link } from 'react-router-dom'
+import Wrapper from '../../assets/wrappers/SharedLayout'
+
 const SharedLayout = () => {
   return (
-    <div>SharedLayout</div>
+    <Wrapper>
+      <nav>
+        <Link to='all-jobs'>all jobs</Link>
+        <Link to='add-job'>all jobs</Link>
+      </nav>
+      <Outlet />
+    </Wrapper>
   )
 }
 export default SharedLayout
