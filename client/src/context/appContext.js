2d92278ae851aab510b87ab6bf48063d8a7e445f
Adel Hasan <adelhasan9947@gmail.com>
2022-07-12 20:14:53 -0500

    persist user information in local storage

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -9,15 +9,20 @@ import {
 } from "./actions";
 import axios from "axios";
 
+const token = localStorage.getItem('token')
+const user = localStorage.getItem('user')
+const userLocation = localStorage.getItem('location')
+
+
 const initialState = {
   isLoading: false,
   showAlert: false,
   alertText: "",
   alertType: "",
-  user: null,
-  token: null,
-  userLocation: "",
-  jobLocation: ""
+  user: user ? JSON.parse(user) : null,
+  token: token,
+  userLocation: userLocation || "",
+  jobLocation: userLocation || ""
 };
 
 const AppContext = React.createContext();
@@ -40,6 +45,18 @@ const AppProvider = ({ children }) => {
     }, 3000);
   };
 
+  const addUserToLocalStorage = ({user, token, location}) => {
+    localStorage.setItem('user', JSON.stringify(user))
+    localStorage.setItem('token', token)
+    localStorage.setItem('location', location)
+  }
+
+  const removeUserFromLocalStorage = () => {
+    localStorage.removeItem('token')
+    localStorage.removeItem('user')
+    localStorage.removeItem('location')
+  }
+
   const registerUser = async (currentUser) => {
     dispatch({ type: REGISTER_USER_BEGIN });
     try {
@@ -53,13 +70,12 @@ const AppProvider = ({ children }) => {
           token,
           location,
         },
-        // will add later
-        // addUserToLocalStorage({
-        //   user,
-        //   token,
-        //   location,
-        // })
       });
+      addUserToLocalStorage({
+        user,
+        token,
+        location,
+      })
     } catch (error) {
       console.log(error.response)
       dispatch({
