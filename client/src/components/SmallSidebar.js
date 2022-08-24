8d4a617bad6e383335fbc08a2e122c1b67992f11
Adel Hasan <adelhasan9947@gmail.com>
2022-08-24 15:05:10 -0500

    small sidebar toggle

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,16 +1,22 @@
-import Wrapper from '../assets/wrappers/SmallSidebar'
-import { FaTimes } from 'react-icons/fa'
-import { useAppContext } from '../context/appContext'
-import links from '../utils/links'
-import { NavLink } from 'react-router-dom'
-import Logo from './Logo'
+import Wrapper from "../assets/wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import { useAppContext } from "../context/appContext";
+import links from "../utils/links";
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
 
 const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
+
   return (
     <Wrapper>
-      <div className="sidebar-container show-sidebar">
+      <div
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
         <div className="content">
-          <button className="close-btn" onClick={() => console.log("toggle")}>
+          <button className="close-btn" onClick={toggleSidebar}>
             <FaTimes />
           </button>
           <header>
@@ -20,7 +26,7 @@ const SmallSidebar = () => {
         </div>
       </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default SmallSidebar
+export default SmallSidebar;
