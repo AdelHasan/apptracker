10c6e3418f4e7520903d7259f6fd616a2933b8af
Adel Hasan <adelhasan9947@gmail.com>
2022-09-12 19:06:39 -0500

    Update user functionality

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,23 @@
+import jwt from 'jsonwebtoken'
 import { UnauthenticatedError } from "../errors/index.js"
 
 const auth = async (req, res, next) => {
+  // check header
   const authHeader = req.headers.authorization
   
-  if (!authHeader) {
+  if (!authHeader || !authHeader.startsWith('Bearer')) {
     throw new UnauthenticatedError('Authentication Failed')
   }
 
-  next()
+  const token = authHeader.split(" ")[1]
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = {userId: payload.userId}
+    next()
+  } catch (error) {
+    throw new UnauthenticatedError('Authentication invalid')
+  }
 }
 
 export default auth
