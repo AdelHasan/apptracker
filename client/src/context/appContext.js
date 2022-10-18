4c8919ff35a49e3c085225304744ef872f38acbb
Adel Hasan <adelhasan9947@gmail.com>
2022-10-18 17:18:20 -0500

    handle change global state update

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -13,14 +13,14 @@ import {
   LOGOUT_USER,
   UPDATE_USER_BEGIN,
   UPDATE_USER_SUCCESS,
-  UPDATE_USER_ERROR
+  UPDATE_USER_ERROR,
+  HANDLE_CHANGE,
 } from "./actions";
 import axios from "axios";
 
-const token = localStorage.getItem('token')
-const user = localStorage.getItem('user')
-const userLocation = localStorage.getItem('location')
-
+const token = localStorage.getItem("token");
+const user = localStorage.getItem("user");
+const userLocation = localStorage.getItem("location");
 
 const initialState = {
   isLoading: false,
@@ -39,7 +39,7 @@ const initialState = {
   jobTypeOptions: ["full-time", "part-time", "remote", "internship"],
   jobType: "full-time",
   statusOptions: ["interview", "declined", "pending"],
-  status: "pending"
+  status: "pending",
 };
 
 const AppContext = React.createContext();
@@ -49,31 +49,31 @@ const AppProvider = ({ children }) => {
 
   // Axios
   const authFetch = axios.create({
-    baseURL: '/api/v1',
-  })
+    baseURL: "/api/v1",
+  });
   // response interceptor
-authFetch.interceptors.request.use(
-  (config) => {
-    config.headers.common['Authorization'] = `Bearer ${state.token}`
-    return config
-  },
-  (error) => {
-    return Promise.reject(error)
-  }
-)
-// response interceptor
-authFetch.interceptors.response.use(
-  (response) => {
-    return response
-  },
-  (error) => {
-    console.log(error.response)
-    if (error.response.status === 401) {
-      logoutUser()
+  authFetch.interceptors.request.use(
+    (config) => {
+      config.headers.common["Authorization"] = `Bearer ${state.token}`;
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
     }
-    return Promise.reject(error)
-  }
-)
+  );
+  // response interceptor
+  authFetch.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      console.log(error.response);
+      if (error.response.status === 401) {
+        logoutUser();
+      }
+      return Promise.reject(error);
+    }
+  );
 
   const displayAlert = () => {
     dispatch({
@@ -90,17 +90,17 @@ authFetch.interceptors.response.use(
     }, 3000);
   };
 
-  const addUserToLocalStorage = ({user, token, location}) => {
-    localStorage.setItem('user', JSON.stringify(user))
-    localStorage.setItem('token', token)
-    localStorage.setItem('location', location)
-  }
+  const addUserToLocalStorage = ({ user, token, location }) => {
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("token", token);
+    localStorage.setItem("location", location);
+  };
 
   const removeUserFromLocalStorage = () => {
-    localStorage.removeItem('token')
-    localStorage.removeItem('user')
-    localStorage.removeItem('location')
-  }
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    localStorage.removeItem("location");
+  };
 
   const registerUser = async (currentUser) => {
     dispatch({ type: REGISTER_USER_BEGIN });
@@ -120,70 +120,88 @@ authFetch.interceptors.response.use(
         user,
         token,
         location,
-      })
+      });
     } catch (error) {
       // console.log(error.response)
       dispatch({
         type: REGISTER_USER_ERROR,
-        payload: { msg: error.response.data.msg }
-      })
+        payload: { msg: error.response.data.msg },
+      });
     }
-    clearAlert()
+    clearAlert();
   };
 
   const loginUser = async (currentUser) => {
-    dispatch({type: LOGIN_USER_BEGIN})
+    dispatch({ type: LOGIN_USER_BEGIN });
     try {
-      const {data} = await axios.post('/api/v1/auth/login', currentUser)
-      const {user, token, location} = data
+      const { data } = await axios.post("/api/v1/auth/login", currentUser);
+      const { user, token, location } = data;
 
       dispatch({
         type: LOGIN_USER_SUCCESS,
-        payload: {user, token, location}
-      })
+        payload: { user, token, location },
+      });
 
-      addUserToLocalStorage({user, token, location})
+      addUserToLocalStorage({ user, token, location });
     } catch (error) {
       dispatch({
         type: LOGIN_USER_ERROR,
-        payload: {msg: error.response.data.msg}
-      })
+        payload: { msg: error.response.data.msg },
+      });
     }
-    clearAlert()
-  }
+    clearAlert();
+  };
 
   const logoutUser = () => {
-    dispatch({ type: LOGOUT_USER })
-    removeUserFromLocalStorage()
-  }
+    dispatch({ type: LOGOUT_USER });
+    removeUserFromLocalStorage();
+  };
 
   const updateUser = async (currentUser) => {
-    dispatch({type: UPDATE_USER_BEGIN})
-    
-    try {
-      const {data} = await authFetch.patch('/auth/updateUser', currentUser,)
-      
-      const {user, location, token} = data
+    dispatch({ type: UPDATE_USER_BEGIN });
 
-      dispatch({type: UPDATE_USER_SUCCESS, payload:{user, location, token}})
-      addUserToLocalStorage({user, location, token})
+    try {
+      const { data } = await authFetch.patch("/auth/updateUser", currentUser);
+
+      const { user, location, token } = data;
+
+      dispatch({
+        type: UPDATE_USER_SUCCESS,
+        payload: { user, location, token },
+      });
+      addUserToLocalStorage({ user, location, token });
     } catch (error) {
       if (error.response.status !== 401) {
         dispatch({
           type: UPDATE_USER_ERROR,
           payload: { msg: error.response.data.msg },
-        })
+        });
       }
     }
-    clearAlert()
-  }
+    clearAlert();
+  };
+
+  const handleChange = ({ name, value }) => {
+    dispatch({ type: HANDLE_CHANGE, payload: { name, value } });
+  };
 
   const toggleSidebar = () => {
-    dispatch({ type: TOGGLE_SIDEBAR })
-  }
+    dispatch({ type: TOGGLE_SIDEBAR });
+  };
 
   return (
-    <AppContext.Provider value={{ ...state, displayAlert, registerUser, loginUser, toggleSidebar, logoutUser, updateUser }}>
+    <AppContext.Provider
+      value={{
+        ...state,
+        displayAlert,
+        registerUser,
+        loginUser,
+        toggleSidebar,
+        logoutUser,
+        updateUser,
+        handleChange
+      }}
+    >
       {children}
     </AppContext.Provider>
   );
