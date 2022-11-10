55912c43602cbb0c00c7eeb5f1198c2383973a71
Adel Hasan <adelhasan9947@gmail.com>
2022-11-10 12:02:27 -0600

    get all job function

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,34 +1,37 @@
-import Job from "../models/Job.js"
-import { StatusCodes } from "http-status-codes"
-import {BadRequestError, NotFoundError} from "../errors/index.js"
+import Job from "../models/Job.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, NotFoundError } from "../errors/index.js";
 
 const createJob = async (req, res) => {
-  const { position, company } = req.body
+  const { position, company } = req.body;
 
   if (!position || !company) {
-    throw new BadRequestError('Please Provide All Values')
+    throw new BadRequestError("Please Provide All Values");
   }
 
-  req.body.createdBy = req.user.userId
+  req.body.createdBy = req.user.userId;
 
-  const job = await Job.create(req.body)
-  res.status(StatusCodes.CREATED).json({ job })
-}
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
+};
 
 const deleteJob = async (req, res) => {
-  res.send('delete job')
-}
+  res.send("delete job");
+};
 
 const getAllJobs = async (req, res) => {
-  res.send('get all jobs')
-}
+  const jobs = await Job.find({ createdBy: req.user.userId });
+  res
+    .status(StatusCodes.OK)
+    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+};
 
 const updateJob = async (req, res) => {
-  res.send('update job')
-}
+  res.send("update job");
+};
 
 const showStats = async (req, res) => {
-  res.send('show stats')
-}
+  res.send("show stats");
+};
 
-export {createJob, deleteJob, getAllJobs, updateJob, showStats}
+export { createJob, deleteJob, getAllJobs, updateJob, showStats };
